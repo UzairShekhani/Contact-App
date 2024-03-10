e4661d39227bc25770089279d9f1fa3e552c4e67
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-03-10 23:49:22 +0500

    UI updated

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { createPortal } from 'react-dom';
+import { MdOutlineClose } from "react-icons/md";
 const Modal = ({onClose,isOpen,Children}) => {
-  return (
+  return createPortal (
     <>
-    {isOpen && <div className=''>Modal</div>}
+       {isOpen &&(
+
+        <>
+    <div className='m-auto z-50 relative min-h-[198px] max-w-[80%] bg-white p-4'>
+        <div className='flex justify-end'>
+            <MdOutlineClose onClick={onClose} className='text-2xl self-end'/>
+        </div>
+    </div>
+    <div onClick={onClose} className='backdrop-blur h-screen w-screen z-40 absolute top-0'/>
     </>
+)}
+    </>
+    ,document.getElementById("modal-root")
   )
 }
 
